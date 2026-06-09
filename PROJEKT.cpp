cf92dd2c6749fded439dddf9b8e413940dd34ebc
Ania <anna_mat@vp.pl>
2026-06-10 01:06:08 +0200

    Add files via upload

---
+++ PROJEKT.cpp
@@ -0,0 +1,15 @@
+﻿#include "Gra.h"
+#include <ctime>
+#include <cstdlib>
+
+#pragma comment(lib, "sfml-graphics.lib")
+#pragma comment(lib, "sfml-window.lib")
+#pragma comment(lib, "sfml-system.lib")
+
+
+int main() {
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    Gra gra;
+    gra.uruchom();
+    return 0;
+}
