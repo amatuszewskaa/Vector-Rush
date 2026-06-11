097d025cf0960721267458f28276326e78cd7699
Ania <anna_mat@vp.pl>
2026-06-11 20:56:55 +0200

    Def struktury klasy Przeszkoda

--- Przeszkoda.h
+++ Przeszkoda.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "Obiekt.h"
 
+//WYMÓG 3: zastosowanie dziedziczenia 
 class Przeszkoda : public Obiekt {
+//WYMÓG 9: podxiał klas 
 private:
     sf::RectangleShape shape;
     sf::Vector2f predkosc;
@@ -9,6 +11,7 @@ private:
 
 public:
     Przeszkoda(float startX, float startY, float predkoscX, float obrot);
+//WYMÓG 4: zastosowanie polimorfizmu(nadpisywanie metod wirtualnych)
     void aktualizuj(float dt) override;
     void rysuj(sf::RenderWindow& win) override;
     sf::FloatRect pobierzGranice() const override;
