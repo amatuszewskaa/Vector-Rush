aa7a5776002d12420de3c8c0c9df010b2ad5a33f
Ania <anna_mat@vp.pl>
2026-06-11 20:42:46 +0200

    Inicjalizacja punktu wejścia aplikacji i konfiguracja globalnego generatora liczb losowych
    Initialize random number generator and use type casting.

--- PROJEKT.cpp
+++ PROJEKT.cpp
@@ -1,13 +1,15 @@
 ﻿#include "Gra.h"
 #include <ctime>
 #include <cstdlib>
-
+//do linkowania biblioteki SFML w visual studio
 #pragma comment(lib, "sfml-graphics.lib")
 #pragma comment(lib, "sfml-window.lib")
 #pragma comment(lib, "sfml-system.lib")
 
 
 int main() {
+    //WYMÓG 14: inicjalizacja generatora liczb pseudolosowych 
+    //WYMÓG 11: zastosowanie rzutowania typów(static_cast)
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
     Gra gra;
     gra.uruchom();
