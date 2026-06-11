0a72892037501af8caed74e6c9c308bc529e0079
Ania <anna_mat@vp.pl>
2026-06-11 19:27:20 +0200

    Def struktury cząsteczek

--- Czasteczka.h
+++ Czasteczka.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "Obiekt.h"
 
+//WYMÓG 3: zastosowanie dziedziczenia 
 class Czasteczka : public Obiekt {
+//WYMÓG 9: podział klas na pola private,protected,public
 private:
     sf::RectangleShape shape;
     sf::Vector2f pozycja;
@@ -12,6 +14,7 @@ private:
 
 public:
     Czasteczka(sf::Vector2f startPoz, sf::Color kol);
+//WYMÓG 4: zastosowanie polimorfizmu
     void aktualizuj(float dt) override;
     void rysuj(sf::RenderWindow& win) override;
     sf::FloatRect pobierzGranice() const override;
