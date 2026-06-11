3d33b911472ca5566ba3b27d7f2572572f55ddec
Ania <anna_mat@vp.pl>
2026-06-11 19:22:52 +0200

    Implementacja mechaniki ruchu opartej na czasie dla obiektów typu Bonus

--- Bonus.cpp
+++ Bonus.cpp
@@ -12,7 +12,9 @@ Bonus::Bonus(float startX, float startY) : czas(0), zebrany(false) {
 
 void Bonus::aktualizuj(float dt) {
     czas += dt;
+    //WYMÓG 10: wykorzystanie prędkości liniowej w pikselach/sekundę(mnożenie przez dt)
     float noweX = shape.getPosition().x + predkoscX * dt;
+    //WYMÓG 18: kreatywne rozwiązanie-ruch obiektu po sinusoidzie
     float noweY = pozycjaStartowa.y + std::sin(czas * 3.f) * 60.f;
     shape.setPosition(noweX, noweY);
 }
